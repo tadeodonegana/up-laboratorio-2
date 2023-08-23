a82108d958c5ba3524c1e8d334e816ab0300a5ef
Tadeo Donegana Braunschweig <tadeodonegana@gmail.com>
2023-08-23 11:10:20 -0300

    End Unidad 1

--- unidad_1.sql
+++ unidad_1.sql
@@ -98,4 +98,100 @@ WHERE E.JOB_ID = (
     WHERE FIRST_NAME = 'John' AND LAST_NAME = 'Smith'
 )
 ORDER BY E.SALARY, E.LAST_NAME
--- Seleccionar los nombres completos, el nombre del departamento y el salario deaquellos empleados que ganan más que el promedio de salarios.
+-- Seleccionar los nombres completos, el nombre del departamento y el salario de aquellos empleados que ganan más que el promedio de salarios.
+SELECT E.FIRST_NAME, E.LAST_NAME, E.SALARY, D.NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+WHERE E.SALARY > (
+    SELECT AVG(SALARY)
+    FROM EMPLOYEE
+)
+-- Mostrar los datos de las órdenes máxima y mínima.
+SELECT *
+FROM SALES_ORDER
+WHERE ORDER_ID IN (
+    SELECT MAX(ORDER_ID)
+    FROM SALES_ORDER
+    UNION
+    SELECT MIN(ORDER_ID)
+    FROM SALES_ORDER
+)
+-- Mostrar la cantidad de órdenes agrupadas por cliente.
+SELECT CUSTOMER_ID, COUNT(ORDER_ID)
+FROM SALES_ORDER
+GROUP BY CUSTOMER_ID
+-- Modificar el ejercicio anterior para desplegar también el nombre y teléfono del cliente.
+SELECT S.CUSTOMER_ID, C.NAME, C.PHONE_NUMBER, COUNT(S.ORDER_ID)
+FROM SALES_ORDER S
+JOIN CUSTOMER C ON S.CUSTOMER_ID = C.CUSTOMER_ID
+GROUP BY S.CUSTOMER_ID, C.NAME, C.PHONE_NUMBER
+-- Mostrar aquellos empleados que tienen dos ó más personas a su cargo.
+SELECT E.FIRST_NAME, E.LAST_NAME, COUNT(E.EMPLOYEE_ID)
+FROM EMPLOYEE E
+JOIN EMPLOYEE M ON E.EMPLOYEE_ID = M.MANAGER_ID
+GROUP BY E.FIRST_NAME, E.LAST_NAME
+HAVING COUNT(E.EMPLOYEE_ID) >= 2
+-- Desplegar el nombre del empleado más antiguo y del empleado más nuevo, (según su fecha de ingreso)
+SELECT FIRST_NAME, LAST_NAME, HIRE_DATE
+FROM EMPLOYEE
+WHERE HIRE_DATE IN (
+    SELECT MIN(HIRE_DATE)
+    FROM EMPLOYEE
+    UNION
+    SELECT MAX(HIRE_DATE)
+    FROM EMPLOYEE
+)
+-- Mostrar la cantidad de empleados que tiene los departamentos 20 y 30.
+SELECT DEPARTMENT_ID, COUNT(EMPLOYEE_ID)
+FROM EMPLOYEE
+WHERE DEPARTMENT_ID IN (20, 30)
+GROUP BY DEPARTMENT_ID
+-- Mostrar el promedio de salarios de los empleados de los departamentos de investigación (Research). Redondear el promedio a dos decimales.
+SELECT ROUND(AVG(SALARY), 2)
+FROM EMPLOYEE
+WHERE DEPARTMENT_ID IN (
+    SELECT DEPARTMENT_ID
+    FROM DEPARTMENT
+    WHERE NAME = 'RESEARCH'
+)
+-- Por cada departamento desplegar su id, su nombre y el promedio de salarios (sin decimales) de sus empleados. El resultado ordenarlo por promedio.
+SELECT D.DEPARTMENT_ID, D.NAME, ROUND(AVG(E.SALARY), 0)
+FROM DEPARTMENT D
+JOIN EMPLOYEE E ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+GROUP BY D.DEPARTMENT_ID, D.NAME
+ORDER BY ROUND(AVG(E.SALARY), 0)
+-- Modificar el ejercicio anterior para mostrar solamente los departamentos que tienenmás de 3 empleados.
+SELECT D.DEPARTMENT_ID, D.NAME, ROUND(AVG(E.SALARY), 0)
+FROM DEPARTMENT D
+JOIN EMPLOYEE E ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+GROUP BY D.DEPARTMENT_ID, D.NAME
+HAVING COUNT(E.EMPLOYEE_ID) > 3
+ORDER BY ROUND(AVG(E.SALARY), 0)
+-- Por cada producto (incluir todos los productos) mostrar la cantidad de unidades que se han pedido y el precio máximo que se ha facturado.
+SELECT P.PRODUCT_ID, P.DESCRIPTION, SUM(I.QUANTITY), MAX(I.UNIT_PRICE)
+FROM PRODUCT P
+JOIN ITEM I ON P.PRODUCT_ID = I.PRODUCT_ID
+GROUP BY P.PRODUCT_ID, P.DESCRIPTION
+-- Para cada cliente mostrar nombre, teléfono, la cantidad de órdenes emitidas y la fecha de su última orden. Ordenar el resultado por nombre de cliente.
+SELECT C.NAME, C.PHONE_NUMBER, COUNT(S.ORDER_ID), MAX(S.ORDER_DATE)
+FROM CUSTOMER C
+JOIN SALES_ORDER S ON C.CUSTOMER_ID = S.CUSTOMER_ID
+GROUP BY C.NAME, C.PHONE_NUMBER
+ORDER BY C.NAME
+-- Para todas las localidades mostrar sus datos, la cantidad de empleados que tiene y el total de salarios de sus empleados. Ordenar por cantidad de empleados.
+SELECT L.LOCATION_ID, L.REGIONAL_GROUP, COUNT(E.EMPLOYEE_ID), SUM(E.SALARY)
+FROM LOCATION L
+JOIN DEPARTMENT D ON L.LOCATION_ID = D.LOCATION_ID
+JOIN EMPLOYEE E ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+GROUP BY L.LOCATION_ID, L.REGIONAL_GROUP
+ORDER BY COUNT(E.EMPLOYEE_ID)
+-- Mostrar los empleados que ganan más que su jefe. El reporte debe mostrar el nombre completo del empleado, su salario, el nombre del departamento al que pertenece y la función que ejerce.
+SELECT E.FIRST_NAME, E.LAST_NAME, E.SALARY, D.NAME, J.FUNCTION
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+JOIN JOB J ON E.JOB_ID = J.JOB_ID
+WHERE E.SALARY > (
+    SELECT SALARY
+    FROM EMPLOYEE
+    WHERE EMPLOYEE_ID = E.MANAGER_ID
+)
